6dcf2b7b1ebc85b791be9f99459811c4671a7d7a
williewu <willie.wu17@gmail.com>
2018-03-12 18:27:17 -0700

    improvements

--- helpers.py
+++ helpers.py
@@ -11,6 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 import time
 import _pickle as pickle
 from random import sample
+import sklearn.metrics as mt
 
 def predict_with_file(filename, dat_tot, start_time, mode, folds):
     '''args: filename: string with the file holding your classifiers
@@ -20,26 +21,33 @@ def predict_with_file(filename, dat_tot, start_time, mode, folds):
             mode: whether multi or binary
        returns: the list of predictions for every row in dat_tot
     '''
+    dat_tot = sample(dat_tot, k=len(dat_tot)) #scramle dat_tot so kfold is legit
     predictions = []
     clf_list = []
-    with open(filename, 'rb') as fid:
-            clf_list = pickle.load(fid)
+    answers = []
+    with open(filename, 'rb') as fid: #open the pkl file with our classifiers and their matching test lesions
+            clf_list, answers = pickle.load(fid)
     for i in range(len(clf_list)):
         test_list = clf_list[i][1]
-        for lesion in test_list:
-            predictions.append(clf_list[i][0].predict([lesion[0][1:],lesion[1][1:]]))
+        for lesion in test_list: #we go through every lesion in the list
+            pred = clf_list[i][0].predict_proba([lesion[0][1:],lesion[1][1:]])
+            max_col = 0; max_i = -1
+            for col in range(pred.shape[1]):
+                if sum(pred[:,col]) > max_col:
+                    max_col = sum(pred[:,col])
+                    max_i = col
+            predictions.append(clf_list[i][0].classes_[max_i])
         if i%5 == 0:
             time_passed = (time.clock()-start_time)/60
-            print (str(int(i)) + " lesions tested in {:.3} minutes".format(time_passed))
-            remaining_time = time_passed/(i+1)*(76-i+1)
+            print (str(int(i*len(test_list))) + " lesions tested in {:.3} minutes".format(time_passed))
+            remaining_time = time_passed/(i*len(test_list)+1)*(76-i*len(test_list)+1)
             print ('Approximately {:.2} minutes remaining'.format(remaining_time))
     time_passed = (time.clock()-start_time)/60
     print('Done in {:.3} minutes'.format(time_passed))
-    return predictions
+    return predictions, answers
 
 def split(row_indcs, folds):
     shuffled_row_indcs = row_indcs
-    #shuffled_row_indcs = sample(row_indcs, k=len(row_indcs)) 
     k, m = divmod(len(row_indcs), folds)
     return (shuffled_row_indcs[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(folds))
 
@@ -51,6 +59,11 @@ def make_classifiers_predict(dat_tot, start_time, folds, mode, n_trees = 1000):
              mode: whether multi or binary
    returns: the list of predictions for every row in dat_tot
     '''
+    dat_tot = sample(dat_tot, k=len(dat_tot)) #scramble the data so kfold is legit    
+    
+    #reads in the answers in a row for each row of the data
+    answers = np.array([dat_tot[i][0] for i in range(0, len(dat_tot)-1, 2)])
+    
     predictions = []
     clf_list = []
     k_fold_list = split(list(range(0,len(dat_tot)-1,2)),folds)
@@ -69,16 +82,22 @@ def make_classifiers_predict(dat_tot, start_time, folds, mode, n_trees = 1000):
         clf.fit(X,y)
         clf_list.append((clf,test_list))
         for lesion in test_list:
-            predictions.append(clf.predict([lesion[0][1:],lesion[1][1:]]))
+            pred = clf.predict_proba([lesion[0][1:],lesion[1][1:]])
+            max_col = 0; max_i = -1
+            for col in range(pred.shape[1]):
+                if sum(pred[:,col]) > max_col:
+                    max_col = sum(pred[:,col])
+                    max_i = col
+            predictions.append(clf.classes_[max_i])
         time_passed = (time.clock()-start_time)/60
         print(str((fold_number+1)*len(test_list)) + " lesions tested in {:.3} minutes".format(time_passed))
         remaining_time = time_passed/((fold_number+1)*len(test_list))*(76-((1+fold_number)*len(test_list)))
         print ('Approximately {:.2} minutes remaining'.format(remaining_time))
     with open(mode + '_' + str(folds) + '_folds_classifiers_trees={}.pkl'.format(n_trees), 'wb') as fid:
-        pickle.dump(clf_list, fid)
+        pickle.dump((clf_list,answers), fid)
     time_passed = (time.clock()-start_time)/60
     print('Done in {:.3} minutes'.format(time_passed))
-    return predictions
+    return predictions, answers
     
 
 def multi_calc_model_stats(predictions, answers):
@@ -90,22 +109,27 @@ def multi_calc_model_stats(predictions, answers):
     '''
     #Hyp == 1 Ser ==2 Aden ==3
     #1 is true positive, true negative, false positive, false negative 
+    confu_matrix = mt.confusion_matrix(answers,predictions, labels = np.array([1,2,3])) 
+    total = np.sum(confu_matrix)
+    print('Confusion Matrix: ')
+    print(confu_matrix)
+    total = np.sum(confu_matrix)
+    
     Hyp = [0,0,0,0]
     Ser = [0,0,0,0]
     Aden = [0,0,0,0]
     Stats = [Hyp,Ser,Aden]
     classes = [0,1,2]
     for predic, ans in zip(predictions, answers):
-        for i in [0,1]:
-            p,a = (int(predic[i]),int(ans[i]))
-            if p == a:
-                Stats[p-1][0] += 1
-                for cl in classes:
-                    if cl is not p:
-                        Stats[cl-1][1] += 1
-            else:
-                Stats[p-1][2] += 1
-                Stats[a-1][3] += 1
+        p,a = (int(predic),int(ans))
+        if p == a:
+            Stats[p-1][0] += 1
+            for cl in classes:
+                if cl is not p:
+                    Stats[cl-1][1] += 1
+        else:
+            Stats[p-1][2] += 1
+            Stats[a-1][3] += 1
     acc = [(c[0]+c[1])/sum(c) for c in Stats]
     sens = [c[0]/(c[0]+c[3]) for c in Stats]
     spec = [c[1]/(c[1]+c[2]) for c in Stats]
@@ -117,17 +141,17 @@ def binary_calc_model_stats(predictions, answers):
        returns: acc: accuracy for the two states
                 sens: sensitivity for the two states
                 spec: specificity for the two states
+                f1: f1 score of the classifier
     '''
     #makes a confusion matrix 
     #[0-actual 0 ,  1-actual 0]
     #[0-actual 1 , 1 - actual 1]
-    confu_matrix = np.zeros((2,2))
-    for predic, ans in zip(predictions, answers):
-        for i in [0,1]:
-            p,a = (int(predic[i]),int(ans[i]))
-            confu_matrix[a,p]+=1
+    confu_matrix = mt.confusion_matrix(answers,predictions, labels = np.array([0,1]))   
     total = np.sum(confu_matrix)
+    print('Confusion Matrix: ')
+    print(confu_matrix)
     acc = float(confu_matrix[0,0]+confu_matrix[1,1])/total
     spec = confu_matrix[0,0]/float(confu_matrix[0,0]+confu_matrix[0,1])
     sens = confu_matrix[1,1]/float(confu_matrix[1,0]+confu_matrix[1,1])
-    return acc, sens, spec
+    f1 = mt.f1_score(answers,predictions)
+    return acc, sens, spec, f1
--- random_forest.py
+++ random_forest.py
@@ -26,7 +26,7 @@ Gastrointestinal Lesion Classifier (by Willie Wu and Linus Chen): Random Forest
 -------------------------------------------------------------------------------''')
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type = str, default='feature_data.csv', help = 'directory holding the data')
-    parser.add_argument('--cmode', type = str, default = 'multi', help = 'chooses binary vs multi classification mode')
+    parser.add_argument('--cmode', type = str, default = 'binary', help = 'chooses binary vs multi classification mode')
     parser.add_argument('--folds', type = int, default = -1, help = 'chooses the amount folds for cross validation')
     parser.add_argument('--new_clfs', type = bool, default=False, help ='when True forces production of new classifiers')
     parser.add_argument('--n_trees', type = int, default = 1000, help ='number of trees in the forest')
@@ -36,12 +36,8 @@ Gastrointestinal Lesion Classifier (by Willie Wu and Linus Chen): Random Forest
     dat_tot = []
     with open(FLAGS.data_path, newline='') as csvfile:
             tot_data = csv.reader(csvfile, delimiter=',')
-            #for indx, row in enumerate(tot_data):
-                #row_in_float = [indx]
-                #for data_pt in row[1:]:
-                    #row_in_float.append(float(data_pt))
             for row in tot_data:
-                dat_tot.append([float(r) for r in row[1:]])
+                dat_tot.append([float(r) for r in row[1:]])#we cut off the zeroth column as all it has is the names of the data
             print('Data read in successfully...')
     start_time = time.clock()
         
@@ -53,8 +49,6 @@ Gastrointestinal Lesion Classifier (by Willie Wu and Linus Chen): Random Forest
                 dat_tot[i][0] = 0.0
     else:
         print('classifying with multiple categories')
-    #reads in the answers in a row for each row of the data
-    answers = np.array([np.array([dat_tot[i][0],dat_tot[i+1][0]]) for i in range(0, len(dat_tot)-1, 2)])
     
     #if we didnt give a fold number then we do LOOCV, change folds to the number of lesions
     if FLAGS.folds == -1:
@@ -64,9 +58,9 @@ Gastrointestinal Lesion Classifier (by Willie Wu and Linus Chen): Random Forest
         
     #if the file is already formed then just use it
     if os.path.isfile(FLAGS.cmode + '_' + str(folds) + '_folds_classifiers_trees={}.pkl'.format(FLAGS.n_trees)) and FLAGS.new_clfs!=True:
-        predictions = predict_with_file(FLAGS.cmode + '_' + str(folds) + '_folds_classifiers_trees={}.pkl'.format(FLAGS.n_trees), dat_tot, start_time, FLAGS.cmode, folds)
+        predictions, answers = predict_with_file(FLAGS.cmode + '_' + str(folds) + '_folds_classifiers_trees={}.pkl'.format(FLAGS.n_trees), dat_tot, start_time, FLAGS.cmode, folds)
     else:
-        predictions = make_classifiers_predict(dat_tot, start_time, folds, FLAGS.cmode, n_trees = FLAGS.n_trees)
+        predictions, answers = make_classifiers_predict(dat_tot, start_time, folds, FLAGS.cmode, n_trees = FLAGS.n_trees)
     
     #we get a bunch of predictions and then we want to convert it into accuracy calcs.
     predictions = np.array(predictions)
@@ -81,14 +75,12 @@ Gastrointestinal Lesion Classifier (by Willie Wu and Linus Chen): Random Forest
             print('Specificity: {0:.2f}%'.format(round(sp*100,2)))
             print('===================')
     else:
-        print(binary_calc_model_stats(predictions,answers))
-        '''
-        acc, sens, spec = binary_calc_model_stats(predictions,answers)
-        for ac, se, sp, name in zip(acc,sens,spec,['benign','malignant']):
-            print(name+ ' stats: ')
-            print('Accuracy: {0:.2f}%'.format(round(ac*100,2)))
-            print('Sensitivity: {0:.2f}%'.format(round(se*100,2)))
-            print('Specificity: {0:.2f}%'.format(round(sp*100,2)))
-            print('===================')
-        '''
+        acc, sens, spec, f1 = binary_calc_model_stats(predictions,answers)
+        print('Binary stats: ')
+        print('Accuracy: {0:.2f}%'.format(round(acc*100,2)))
+        print('Sensitivity: {0:.2f}%'.format(round(sens*100,2)))
+        print('Specificity: {0:.2f}%'.format(round(spec*100,2)))
+        print('F1-Score: {0:.2f}'.format(round(f1,2)))
+        print('===================')
+        
     
