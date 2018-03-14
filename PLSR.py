bd1a2abc636e23e1bf0095cdd7fe9d510b2bff29
yinyinlbc <u6.30cl@gmail.com>
2018-03-14 08:53:10 -0700

    Binary Works
    yes

--- PLSR.py
+++ PLSR.py
@@ -10,6 +10,8 @@ import numpy as np
 import csv
 import argparse
 from random import shuffle
+
+import sys
 from helpers import *
 
 #move to helpers.py later
@@ -21,7 +23,7 @@ class FlagError(Exception):
 parser = argparse.ArgumentParser()
 parser.add_argument('--cmode', type = str, default = 'binary', help = 'chooses binary vs multi classification mode')
 parser.add_argument('--folds', type = int, default = -1, help = 'chooses the amount folds for cross validation. Default is LOOCV')
-parser.add_argument('--components', type = int, default = 25, help = 'number of principal components in the model')
+parser.add_argument('--components', type = int, default = 30, help = 'number of principal components in the model')
 FLAGS = parser.parse_args()
 
 #load data
@@ -46,14 +48,16 @@ classes = ['Hyp (b)', 'Ser (m)', 'Ade (m)']
 model = skcd.PLSRegression(FLAGS.components)
 
 #pair indicates that the 2 rows were combined into one
-pair_data = [feature_data[i][1:] + feature_data[i+1][1:] for i in range(0, len(feature_data)-1, 2)]
+pair_data = np.array([feature_data[i][1:] + feature_data[i+1][1:] for i in range(0, len(feature_data)-1, 2)])
+pair_answers = np.array([feature_data[i][0] for i in range(0, len(feature_data)-1, 2)])
 pair_modified_answers = []
 
 #multiclass prediction is 3 runs of PLSR-DA, for x vs. not-x
 #for binary, only check the first run of hyp vs. not-hyp
 #3 different sets of answers for each type of classification
 for i in range(len(classes)):
-        pair_modified_answers.append([1 if feature_data[j][0] == (i+1) else -1 for j in range(0,len(feature_data)-1, 2)])
+    pair_modified_answers.append([1 if pair_answers[j] == (i+1) else -1 for j in range(0,len(pair_answers))])
+pair_modified_answers = np.array(pair_modified_answers)
 
 fold_quantity = FLAGS.folds if FLAGS.folds != -1 else len(pair_data)
 
@@ -64,31 +68,38 @@ shuffle(fold_membership)
 for b in range(len(fold_membership)):
         folds[fold_membership[b]].append(b)
 
+
 #make predictions for each fold and each class
 test_pair_predictions = []
+test_pair_answers = []
 for f in range(fold_quantity):
     
     fold_test_pair_predictions = []
+    fold_test_pair_answers = []
+    test_fold = folds[f]
+    train_fold = [i for i in range(len(pair_data)) if i not in test_fold]
+    
     for c in range(len(classes)):
         
         #set training data
-        training_pair_data = pair_data[folds[f]]
-        training_pair_modified_answers = pair_modified_answers[c][folds[f]]
+        train_pair_data = pair_data[train_fold]
+        train_pair_modified_answers = pair_modified_answers[c][train_fold]
         
         #set test data
-        test_fold = [i for i in range(len(pair_data)) if i not in folds[f]]
         test_pair_data = pair_data[test_fold]
         test_pair_modified_answers = pair_modified_answers[c][test_fold]
         
         #fit model to training data, predict
-        model.fit(training_pair_data, training_pair_modified_answers)
-        fold_test_pair_predictions.append(np.squeeze(model.predict(test_pair_data)))
-        
-    test_pair_predictions.append(fold_test_pair_predictions)
-    
+        model.fit(train_pair_data, train_pair_modified_answers)
+        fold_test_pair_predictions.append(model.predict(test_pair_data).flatten())
+
+    test_pair_predictions.append(np.swapaxes(fold_test_pair_predictions,0,1))
+    test_pair_answers.append(pair_answers[test_fold])
+
 #find which class has the highest predicted value
 if FLAGS.cmode == 'binary':
-    predictions = [[1 if l[0] > cutoff[0] else 0 for l in m] for m in test_pair_predictions]
+    predictions = [1 if ff[0] > 0 else 0 for f in test_pair_predictions for ff in f]
+    answers = [1 if aa == 1 else 0 for a in test_pair_answers for aa in a]
     acc, sens, spec, f1 = binary_calc_model_stats(predictions, answers)
     print('Accuracy: {0:.2f}%'.format(round(acc*100,2)))
     print('Sensitivity: {0:.2f}%'.format(round(sens*100,2)))
@@ -97,6 +108,8 @@ if FLAGS.cmode == 'binary':
     print('===================')
 
 if FLAGS.cmode == 'multi':
+    predictions = [np.argmax(f) + 1 for f in test_pair_predictions for ff in f]
+    answers = test_pair_answers
     for i in range(len(predictions)):
         acc, sens, spec = multi_calc_model_stats(predictions[i], answers)
         for ac, se, sp, name in zip(acc,sens,spec,['Hyperplasic','Serrated','Adenoma']):
